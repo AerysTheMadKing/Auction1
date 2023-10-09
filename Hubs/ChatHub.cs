224a57c0a42760de384748ffcd0cf476a3e3bb13
Your Nuuman <nuumanisaev@example.com>
2023-10-09 08:24:03 +0600

    InitialCreate

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Auction1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message, string user)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using Auction1.Data;
+using Auction1.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace SignalRServerIdentityAuthentication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddSignalR();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] {"application/octet-stream "});
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapHub<ChatHub>("/chatHub");
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
